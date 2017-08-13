1125f89f159aa52fa02244259d5c595d54790332
jiuyang <lunineep@gmail.com>
2017-08-13 17:07:29 +0800

    delete some file

--- test/continue.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-n = 0
-while n < 10:
-    n = n + 1
-    if n % 2 ++ 0:   #如果n是偶数，执行continue
-        continue     #continue直接继续下个循环，跳过后边的print（n）
-    print(n)
--- test/for.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-#打印list
-names = ['Michael', 'Bob', 'Tracy']
-for name in names:
-    print(name)
-
-#打印数字0-9
-for x in range(10):
-    print(x)
