059bd4089d8d7ff35611fa20f25fdff60770ef8a
nineep <lunineep@gmail.com>
2018-07-27 10:56:33 +0800

    Delete old files

--- python_practice/send_mail.py
+++
@@ -1,9 +0,0 @@
-import smtplib
-smtp = smtplib.SMTP()
-smtp.connect("smtp.qq.com", "25")
-smtp.login('账号', '密码')
-origHdrs = ['From:nineep@qq.com', 'To:nineep@foxmail.com', 'Subject: test msg']
-origBody = ["This is by python send mail demo!"]
-smtp.sendmail('nineep@qq.com', 'nineep@foxmail.com',
-              "\r\n\r\n".join(["\r\n"join(origHdrs), "\r\n".join(origBody)]))
-smtp.quit()
