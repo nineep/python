e3c0ab9e7e25fa46856c7fd759f8ba2d820d62cd
nineep <lunineep@gmail.com>
2018-08-03 15:25:08 +0800

    Some flask-web examples

---
+++ flask/hello_flask_bootsrap.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+from flask.ext.script import Manager
+from flask import render_template
+from flask.ext.bootsrap import Bootstrap
+
+
+app = Flask(__name__)
+manager = Manager(app)
+bootsrap = Bootstrap(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+if __name__ == '__main__':
+#    app.run(debug=True)
+    manager.run()
